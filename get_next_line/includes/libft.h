f47ddc811ef4d6aef5b3afd1de5fed4ab86d7084
Tiago manuel Ferreira nevoa <tferreir@c1r21s7.42lisboa.com>
2022-04-21 12:27:51 +0100

    MakeFile structure and .h

---
+++ get_next_line/includes/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tferreir <tferreir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 16:30:02 by tiago_nevoa       #+#    #+#             */
+/*   Updated: 2022/04/21 12:14:49 by tferreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+# define LIBFT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlen(const char *s);
+
+#endif
